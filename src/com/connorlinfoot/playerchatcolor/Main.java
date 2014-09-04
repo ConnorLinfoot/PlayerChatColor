d4695fc868f059c156e988c1f09edcd53ca2a672
Connor Linfoot <connorlinfoot@me.com>
2014-09-04 15:17:17 +0100

    More colors and also support for changing other players color

--- src/com/connorlinfoot/playerchatcolor/Main.java
+++ src/com/connorlinfoot/playerchatcolor/Main.java
@@ -1,5 +1,6 @@
 package com.connorlinfoot.playerchatcolor;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
@@ -32,7 +33,7 @@ public class Main extends JavaPlugin implements Listener {
         ConsoleCommandSender console = server.getConsoleSender();
 
         console.sendMessage(ChatColor.GREEN + "========= PlayerChatColor! =========");
-        console.sendMessage(ChatColor.GREEN + "=========== VERSION: 1.0 ===========");
+        console.sendMessage(ChatColor.GREEN + "=========== VERSION: 1.1 ===========");
         console.sendMessage(ChatColor.GREEN + "======== BY CONNOR LINFOOT! ========");
     }
 
@@ -46,22 +47,44 @@ public class Main extends JavaPlugin implements Listener {
             sender.sendMessage(Prefix + "PlayerChatColor is currently disabled");
             return false;
         }
+        Player p = null;
 
-        if( !( sender instanceof Player) ) {
-            sender.sendMessage(Prefix + "PlayerChatColor must be ran as a player");
-            return false;
-        }
-
-        Player p = (Player) sender;
-
-        if( Permissions ){
-            if( !p.hasPermission("chatcolor.use") ){
-                sender.sendMessage(Prefix + "You dont have the permission \"chatcolor.use\"");
+        if( args.length == 2 ) {
+            p = Bukkit.getPlayer(args[0]);
+            if( Permissions ){
+                if( sender instanceof Player ) {
+                    Player pp = (Player) sender;
+                    if (!pp.hasPermission("chatcolor.other")) {
+                        sender.sendMessage(Prefix + "You dont have the permission \"chatcolor.other\"");
+                        return false;
+                    }
+                }
+            } else if( !sender.isOp() ){
+                sender.sendMessage(Prefix + "Your not op");
                 return false;
+            }
+        } else {
+            if( !( sender instanceof Player) ) {
+                sender.sendMessage(Prefix + "PlayerChatColor must be ran as a player");
+                return false;
+            }
+
+            p = (Player) sender;
+
+            if( Permissions ){
+                if( !p.hasPermission("chatcolor.use") ){
+                    sender.sendMessage(Prefix + "You dont have the permission \"chatcolor.use\"");
+                    return false;
+                }
             }
         }
 
-        if( args.length == 1 ){
+        if( p == null ){
+            sender.sendMessage(Prefix + "Player not found");
+            return false;
+        }
+
+        if( args.length >= 1 ){
 
             if( args[0].equalsIgnoreCase("list") ){
                 // List of all colors
@@ -71,35 +94,50 @@ public class Main extends JavaPlugin implements Listener {
                 sender.sendMessage(Prefix + ChatColor.AQUA + "- aqua");
                 sender.sendMessage(Prefix + ChatColor.RED + "- red");
                 sender.sendMessage(Prefix + ChatColor.BLUE + "- blue");
+                sender.sendMessage(Prefix + ChatColor.GOLD + "- gold");
+                sender.sendMessage(Prefix + ChatColor.YELLOW + "- yellow");
+                sender.sendMessage(Prefix + ChatColor.LIGHT_PURPLE + "- purple");
                 sender.sendMessage(Prefix + "More coming soon");
                 return true;
             }
 
             if( args[0].equalsIgnoreCase("white") ){
                 setPlayerChatColor(p, ChatColor.WHITE);
-                sender.sendMessage(Prefix + "Your ChatColor has been updated");
+                sender.sendMessage(Prefix + "ChatColor has been updated");
                 return true;
             } else if( args[0].equalsIgnoreCase("green") ){
                 setPlayerChatColor(p, ChatColor.GREEN);
-                sender.sendMessage(Prefix + "Your ChatColor has been updated");
+                sender.sendMessage(Prefix + "ChatColor has been updated");
                 return true;
             } else if( args[0].equalsIgnoreCase("aqua") ){
                 setPlayerChatColor(p, ChatColor.AQUA);
-                sender.sendMessage(Prefix + "Your ChatColor has been updated");
+                sender.sendMessage(Prefix + "ChatColor has been updated");
                 return true;
             } else if( args[0].equalsIgnoreCase("red") ){
                 setPlayerChatColor(p, ChatColor.RED);
-                sender.sendMessage(Prefix + "Your ChatColor has been updated");
+                sender.sendMessage(Prefix + "ChatColor has been updated");
                 return true;
             } else if( args[0].equalsIgnoreCase("blue") ){
                 setPlayerChatColor(p, ChatColor.BLUE);
-                sender.sendMessage(Prefix + "Your ChatColor has been updated");
+                sender.sendMessage(Prefix + "ChatColor has been updated");
+                return true;
+            } else if( args[0].equalsIgnoreCase("gold") ){
+                setPlayerChatColor(p, ChatColor.GOLD);
+                sender.sendMessage(Prefix + "ChatColor has been updated");
+                return true;
+            } else if( args[0].equalsIgnoreCase("yellow") ){
+                setPlayerChatColor(p, ChatColor.YELLOW);
+                sender.sendMessage(Prefix + "ChatColor has been updated");
+                return true;
+            } else if( args[0].equalsIgnoreCase("purple") ){
+                setPlayerChatColor(p, ChatColor.LIGHT_PURPLE);
+                sender.sendMessage(Prefix + "ChatColor has been updated");
                 return true;
             }
 
         }
 
-        sender.sendMessage(Prefix + ChatColor.RED + "Usage: /chatcolor <color/list>");
+        sender.sendMessage(Prefix + ChatColor.RED + "Usage: /chatcolor [player] <color/list>");
         return true;
     }
 
